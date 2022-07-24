e6c052dd16764f164854d396b5d4b721f613a1fb
Lauri Jesmin <lauri.jesmin@gmail.com>
2022-07-24 19:08:17 +0300

    experiment

--- Classes/Domain/Finishers/FormvouchersFinisher.php
+++ Classes/Domain/Finishers/FormvouchersFinisher.php
@@ -2,13 +2,7 @@
 namespace Brightside\Formvouchers\Domain\Finishers;
 
 use Brightside\Formvouchers\Domain\Model\Formvouchers;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Mail\MailMessage;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
-use TYPO3\CMS\Fluid\View\StandaloneView;
-use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
-use TYPO3\CMS\Form\Domain\Runtime\FormRuntime;
+use TYPO3\CMS\Form\Domain\Model\Renderable\AbstractRenderable;
 
 class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher
 {
@@ -20,14 +14,14 @@ class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFini
         $voucher = "Experiment";
 
         /** @var AbstractRenderable $newField */
-        // $newField = $this->finisherContext
-        //     ->getFormRuntime()
-        //     ->getFormDefinition()
-        //     ->getPageByIndex(0)
-        //     ->createElement('code', 'Text');
-        // $newField->setDefaultValue($voucher);
-        // $newField->setDataType('string');
-        // $newField->setLabel('code');
+        $newField = $this->finisherContext
+            ->getFormRuntime()
+            ->getFormDefinition()
+            ->getPageByIndex(0)
+            ->createElement('code', 'Text');
+        $newField->setDefaultValue($voucher);
+        $newField->setDataType('string');
+        $newField->setLabel('code');
 
         $this->finisherContext->getFinisherVariableProvider()->add(
             $this->shortFinisherIdentifier,
