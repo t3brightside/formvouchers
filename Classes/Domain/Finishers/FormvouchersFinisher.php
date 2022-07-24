c2439eea7d712bff7f4483911c3c68e4d85fabaa
Lauri Jesmin <lauri.jesmin@gmail.com>
2022-07-24 18:48:46 +0300

    Finisher update

--- Classes/Domain/Finishers/FormvouchersFinisher.php
+++ Classes/Domain/Finishers/FormvouchersFinisher.php
@@ -10,7 +10,7 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
 use TYPO3\CMS\Form\Domain\Runtime\FormRuntime;
 
-class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\EmailFinisher
+class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher
 {
     /**
      * @return string|null
