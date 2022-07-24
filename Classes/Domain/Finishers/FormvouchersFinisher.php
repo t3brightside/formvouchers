3f1abc94e0fa7e8380f9cfdc15d8f313111922ab
Lauri Jesmin <lauri.jesmin@gmail.com>
2022-07-24 18:50:44 +0300

    test

--- Classes/Domain/Finishers/FormvouchersFinisher.php
+++ Classes/Domain/Finishers/FormvouchersFinisher.php
@@ -20,14 +20,14 @@ class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFini
         $voucher = "Experiment";
 
         /** @var AbstractRenderable $newField */
-        $newField = $this->finisherContext
-            ->getFormRuntime()
-            ->getFormDefinition()
-            ->getPageByIndex(0)
-            ->createElement('code', 'Text');
-        $newField->setDefaultValue($voucher);
-        $newField->setDataType('string');
-        $newField->setLabel('code');
+        // $newField = $this->finisherContext
+        //     ->getFormRuntime()
+        //     ->getFormDefinition()
+        //     ->getPageByIndex(0)
+        //     ->createElement('code', 'Text');
+        // $newField->setDefaultValue($voucher);
+        // $newField->setDataType('string');
+        // $newField->setLabel('code');
 
         $this->finisherContext->getFinisherVariableProvider()->add(
             $this->shortFinisherIdentifier,
