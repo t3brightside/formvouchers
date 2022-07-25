38afba53fb0296a5dc1763ac117b955977fffdb3
Lauri Jesmin <lauri.jesmin@gmail.com>
2022-07-25 17:32:47 +0300

    Lukustamine

--- Classes/Domain/Finishers/FormvouchersFinisher.php
+++ Classes/Domain/Finishers/FormvouchersFinisher.php
@@ -23,44 +23,57 @@ class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFini
         $voucherPageUid = $this->parseOption('voucherPageUid');
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_formvouchers_domain_model_vouchers');
 
-        $row = $queryBuilder
-            ->select('uid', 'voucher')
-            ->from('tx_formvouchers_domain_model_vouchers')
-            ->where(
-                $queryBuilder->expr()->eq('is_used', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT))
-            )
-            ->setMaxResults(1)
-            ->executeQuery()
-            ->fetchAssociative();
-
-        $uid = $row['uid'];
-        $voucher = $row['voucher']. " " . $uid;
-
-        $queryBuilder
-            ->update('tx_formvouchers_domain_model_vouchers')
-            ->set('is_used', 1)
-            ->set('pid', $voucherPageUid)
-            ->where(
-                $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter((int)$uid, \PDO::PARAM_INT))
+        $key = 456123;
+        // This is Old School
+        $semaphore = sem_get($key);
+        $waitCount = 0;
+        $hasSemaphore=sem_acquire($semaphore, true);
+        while (!$hasSemaphore and $waitCount < 10) {
+            sleep(1);
+            $waitCount++;
+            $hasSemaphore=sem_acquire($semaphore, true);
+        }
+        if ($hasSemaphore) {
+            $row = $queryBuilder
+                ->select('uid', 'voucher')
+                ->from('tx_formvouchers_domain_model_vouchers')
+                ->where(
+                    $queryBuilder->expr()->eq('is_used', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT))
                 )
-            ->execute();
+                ->setMaxResults(1)
+                ->executeQuery()
+                ->fetchAssociative();
 
-        /** @var AbstractRenderable $newField */
-        $newField = $this->finisherContext
-            ->getFormRuntime()
-            ->getFormDefinition()
-            ->getPageByIndex(0)
-            ->createElement('code', 'Text');
-        $newField->setDefaultValue($voucher);
-        $newField->setDataType('string');
-        $newField->setLabel('code');
+            $uid = $row['uid'];
+            $voucher = $row['voucher']. " " . $uid;
 
-        $this->finisherContext->getFinisherVariableProvider()->add(
-            $this->shortFinisherIdentifier,
-            'code',
-            $voucher
-        );
+            $queryBuilder
+                ->update('tx_formvouchers_domain_model_vouchers')
+                ->set('is_used', 1)
+                ->set('pid', $voucherPageUid)
+                ->where(
+                    $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter((int)$uid, \PDO::PARAM_INT))
+                    )
+                ->execute();
 
+            sem_release($semaphore);
+
+            /** @var AbstractRenderable $newField */
+            $newField = $this->finisherContext
+                ->getFormRuntime()
+                ->getFormDefinition()
+                ->getPageByIndex(0)
+                ->createElement('code', 'Text');
+            $newField->setDefaultValue($voucher);
+            $newField->setDataType('string');
+            $newField->setLabel('code');
+
+            $this->finisherContext->getFinisherVariableProvider()->add(
+                $this->shortFinisherIdentifier,
+                'code',
+                $voucher
+            );
+        }
         return null;
     }
 }
