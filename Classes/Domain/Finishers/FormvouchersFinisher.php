3436b0982398cf4d2928cc85915aeb95ce3c1985
Lauri Jesmin <lauri.jesmin@gmail.com>
2022-07-25 15:51:47 +0300

    Katse

--- Classes/Domain/Finishers/FormvouchersFinisher.php
+++ Classes/Domain/Finishers/FormvouchersFinisher.php
@@ -35,11 +35,11 @@ class FormvouchersFinisher extends \TYPO3\CMS\Form\Domain\Finishers\AbstractFini
         $uid = $row['uid'];
         $voucher = $row['voucher']. " " . $uid;
 
-        // $queryBuilder->update('tx_formvouchers_domain_model_vouchers')
-        //     ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($uid, \PDO::PARAM_INT)))
-        //     ->set('is_used', 1)
-        //     ->set('pid', $voucherPageUid)
-        //     ->executeQuery();
+        $queryBuilder->update('tx_formvouchers_domain_model_vouchers')
+            ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($uid, \PDO::PARAM_INT)))
+            ->set('is_used', 1)
+            // ->set('pid', $voucherPageUid)
+            ->executeQuery();
 
         /** @var AbstractRenderable $newField */
         $newField = $this->finisherContext
