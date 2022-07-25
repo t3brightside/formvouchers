bc4a17a9f12bd5229af05fc5d9da9c6b50d236f6
Tanel Põld <tanel@brightside.ee>
2022-07-25 10:00:01 +0300

    [TASK] Rename vouchers database table

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,7 @@
 #
 # Table structure for table 'tx_formvouchers'
 #
-CREATE TABLE tx_formvouchers (
+CREATE TABLE tx_formvouchers_domain_model_vouchers (
     uid int(11) unsigned DEFAULT 0 NOT NULL auto_increment,
     pid int(11) DEFAULT 0 NOT NULL,
 
